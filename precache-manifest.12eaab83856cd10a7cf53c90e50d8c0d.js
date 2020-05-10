87302e6073c155c421dda76d3c024578e28a0209
Vladislav Yaremchuk <VYarem4yk@gmail.com>
2020-05-10 22:55:16 +0300

    Updates

---
+++ precache-manifest.12eaab83856cd10a7cf53c90e50d8c0d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9808a178cc5f7c72f1944b4a88679f9e",
+    "url": "/Calculator-01/index.html"
+  },
+  {
+    "revision": "ee2e2adebff296f1f62e",
+    "url": "/Calculator-01/static/css/main.9718ad52.chunk.css"
+  },
+  {
+    "revision": "519feb113464b272d05c",
+    "url": "/Calculator-01/static/js/2.14480b90.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Calculator-01/static/js/2.14480b90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee2e2adebff296f1f62e",
+    "url": "/Calculator-01/static/js/main.1746f6c8.chunk.js"
+  },
+  {
+    "revision": "14d4c7213f42593cabd1",
+    "url": "/Calculator-01/static/js/runtime-main.1d60fdf0.js"
+  }
+]);
